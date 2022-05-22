1f30b3d7153afbc3ad5e40222ff183408db71825
SAWADA Tadashi <cesare@mayverse.jp>
2022-05-22 16:14:28 +0900

    Setup mail content

--- app/mailers/author_mailer.rb
+++ app/mailers/author_mailer.rb
@@ -1,12 +1,6 @@
 class AuthorMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.author_mailer.welcome.subject
-  #
   def welcome
-    @greeting = "Hi"
+    @author = params[:author]
 
     mail to: "to@example.org"
   end
